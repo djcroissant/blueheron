c0c87764ba6c3e360792bcd803844bc855acc252
Derek Covey <derek.covey@gmail.com>
2017-01-06 09:49:18 -0800

    Refactor title per page using helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_name = "")
+    base_title = "Blue Heron Marketplace"
+    return base_title if page_name.blank?
+    "#{page_name} | #{base_title}"
+  end
 end
