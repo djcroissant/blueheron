d8ea762743d0ed1abeec5d562a9abc369ab466f1
Derek Covey <derek.covey@gmail.com>
2017-01-11 08:47:49 -0800

    Add users sign up form and tests

---
+++ test/integration/users_signup_test_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name: " ",
+                               email: "user@invalid",
+                               password: "foo",
+                               password_confirmation: "bar" }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+    assert_not flash.nil?
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name: "Example User",
+                                            email: "val@id.com",
+                                            password: "Foobar22",
+                                            password_confirmation: "Foobar22"}
+    end
+    assert_template 'users/show'
+    assert_not flash.nil?
+  end
+end
