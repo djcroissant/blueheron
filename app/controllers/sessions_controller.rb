6a9886da0175ed8df1e49c8451a6197b2ca2c996
Derek Covey <derek.covey@gmail.com>
2017-01-16 10:48:56 -0800

    Add cookies for persistent session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: session_params[:email].downcase)
     if user && user.authenticate(session_params[:password])
       log_in user
+      remember user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
@@ -14,7 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     flash[:success] = 'You have been successfully logged out'
     redirect_to root_url
   end
